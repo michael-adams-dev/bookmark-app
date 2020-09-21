933925670322a1bf52e70c181ecc1f047e7f73b1
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-20 19:26:19 -0700

    added create bookmark to router method

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,18 +3,26 @@ class Menu
     @bookmarks = []
   end
 
-
-
   def display_menu
-    puts "Welcome to the bookmarks app!"
-    puts "1. View all bookmarks"
-    puts "2. Create new bookmark"
-    puts "3. Exit the app"
+    puts 'Welcome to the bookmarks app!'
+    puts '1. View all bookmarks'
+    puts '2. Create new bookmark'
+    puts '3. Exit the app'
   end
 
   def selection
-    print "> "
+    print '> '
     gets.chomp.to_i
+  end
+
+  def create_bookmark 
+    bookmark = Bookmark.bookmark_user_input
+    @bookmarks << Bookmark.new(
+      bookmark[:title],
+      bookmark[:url],
+      bookmark[:tags],
+      bookmark[:description]
+    )
   end
 
   def router
@@ -22,9 +30,9 @@ class Menu
       display_menu
       case selection
       when 1
-        # do something
+        p @bookmarks
       when 2
-        # do something
+        create_bookmark
       when 3
         exit
       end
