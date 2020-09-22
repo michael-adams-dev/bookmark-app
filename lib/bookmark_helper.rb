cfb08f422f124931da1b8a31ead1f0f885cb289e
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-21 19:39:41 -0700

    added bookmarks_db constant

--- lib/bookmark_helper.rb
+++ lib/bookmark_helper.rb
@@ -1,4 +1,5 @@
 module BookmarkHelper
   INPUTS = %i[title url tags description].freeze
   HEADINGS = %i[id title url tags description].freeze
+  BOOKMARKS_DB = Dir.pwd + "/public/bookmarks.json"
 end
