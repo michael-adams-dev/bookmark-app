0fb0a50b5bb8a6f8e69c722cbfbc9b171be47117
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-21 21:41:39 -0700

    added tty-prompt and fixed menu

--- lib/bookmark_helper.rb
+++ lib/bookmark_helper.rb
@@ -2,4 +2,5 @@ module BookmarkHelper
   INPUTS = %i[title url tags description].freeze
   HEADINGS = %i[id title url tags description].freeze
   BOOKMARKS_DB = Dir.pwd + "/public/bookmarks.json"
+  PROMPT = TTY::Prompt.new
 end
