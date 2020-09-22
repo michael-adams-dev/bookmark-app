e7d7e62c638a0ee8ba7bf39011c91ccff4c1b3ff
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-21 19:42:16 -0700

    required bookmark repo file

--- lib/run.rb
+++ lib/run.rb
@@ -2,7 +2,7 @@ require 'terminal-table'
 require_relative 'menu'
 require_relative 'bookmark_helper'
 require_relative 'bookmark'
+require_relative 'bookmark-repository'
 
 app = Menu.new
-
 app.router
